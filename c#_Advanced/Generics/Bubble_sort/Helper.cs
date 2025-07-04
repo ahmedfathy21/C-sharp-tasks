0b05cb2d08416de44975d4eacd83d4bea06abd8c
Ahmed Fathy <ahmedfathy11876@gmail.com>
2025-07-04 21:37:43 +0300

    New project refer to bubble sort integrated with the generic

---
+++ c#_Advanced/Generics/Bubble_sort/Helper.cs
@@ -0,0 +1,46 @@
+namespace Is_As;
+
+public class Helper<T> where T : IEquatable<T>  , IComparable<T>
+
+{
+        public static int Linear_Search(T[] arr, T value ,EqualityComparer<T> employeeComparer)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                // if (arr[i].Equals(value))
+                if (employeeComparer.Equals(arr[i], value))
+                {
+                    return i;
+    
+                }
+            }
+            return -1;
+        }
+
+    public static void Bouble_sort(T[]? array)
+    {
+
+        if (array is not null)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (array[j].CompareTo(array[j+1]) > 0)
+                        Swap(ref array[j], ref  array[j+1]);
+                }
+            }
+            
+        }
+      
+        
+    }
+
+    public static void Swap(ref T a, ref T b)
+    {
+        T temp  = a;
+        a = b;
+        b = temp;
+    }
+ 
+}
---
+++ c#_Advanced/Generics/Bubble_sort/Program.cs
@@ -0,0 +1,27 @@
+﻿using Is_As;
+
+namespace Bubble_sort;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        student[] students =
+            {
+                new student("John", 20, "cs"),
+                new student("Jane", 23, "phy"),
+                new student("Jake", 25, "math"),
+                new student("Jill", 18, "cardio"),
+                new student("Jake", 28, "cash"),
+
+            };
+        Helper<student>.Bouble_sort(students);
+        foreach (var student in students)
+        {
+            Console.WriteLine(student);
+        }
+    }
+
+    
+}
