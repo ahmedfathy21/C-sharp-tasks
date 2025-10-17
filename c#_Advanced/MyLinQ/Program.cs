8fa36782d6351754f0078728c6a8f6513086c709
Ahmed Fathy <ahmedfathy11876@gmail.com>
2025-10-17 10:10:34 +0300

    Add MyLinQ project with LINQ syntax examples and execution concepts
    - Added MyLinQ.csproj and MyLinQ.sln for project setup
    - Updated Program.cs with comprehensive LINQ examples
    - Demonstrated both Fluent and Query syntax
    - Included examples of deferred vs immediate execution
    - Added examples showing var, dynamic, and anonymous types

--- c#_Advanced/MyLinQ/Program.cs
+++ c#_Advanced/MyLinQ/Program.cs
@@ -4,31 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        #region C# KeyWords
+        // #region C# KeyWords
 
-        // The var keyword don`t has something special like int , string 
-        // can`t be initialized as a null or not initialized must be initialized 
-        // not most properly secured , Implicitly data type
-        var name = "Ahmed";
+        // // // The var keyword don`t has something special like int , string 
+        // // // can`t be initialized as a null or not initialized must be initialized 
+        // // // not most properly secured , Implicitly data type
+        // // var name = "Ahmed";
+        //
+        // // // dynamic don`t need have to be defined 
+        // // dynamic name2 = "Ahmed";
+        //
+        //
+        // // // anonymous Type 
+        // // var person = new { name = "Ahmed", age = 20 };
+        // // var person2 = new { Name = "Ahmed", age = 20 };
+        // // Console.WriteLine(person.Equals(person2));
+        // // Console.WriteLine(person.GetType());
+        //
+        // /* person.name = "Fatma";   no option to change the Value of the anonymous data type after th  e creation */
 
-        // dynamic don`t need have to be defined 
-        dynamic name2 = "Ahmed"; 
-        
-        
-        // anonymous Type 
-         var person = new { name = "Ahmed", age = 20 };
-         var person2 = new { Name = "Ahmed", age = 20 };
-         Console.WriteLine(person.Equals(person2));
-         Console.WriteLine(person.GetType());
-         
-         /* person.name = "Fatma";   no option to change the Value of the anonymous data type after th  e creation */
-         
-        
 
-         
-         
 
-        #endregion
+
+
+
+        // #endregion
 
         #region LINQ!
 
@@ -36,18 +36,36 @@ class Program
 
         /* Fluent syntax Methods as a class member from Enumerable */
         List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        var result=numbers.Where(number => number % 2 == 0);
+        var result = numbers.Where(number => number % 2 == 0);
         foreach (var item in result)
             Console.WriteLine(item);
-        
+
 
         #endregion
 
         #region Query Syntax
         /* Easier when using Join Or GroupBy */
         var result01 = from N in numbers where N % 2 != 0 select N;
-        foreach(var item in result01)
+        foreach (var item in result01)
             Console.WriteLine(item);
+
+
+        #endregion
+        #region Linq Execution
+        /* Deferred Execution */
+        /* The query is not executed until you iterate over it */
+        var query = from N in numbers where N % 2 == 0 select N;
+        numbers.Add(12); // Adding a new number to the list
+        foreach (var item in query)
+            Console.WriteLine(item);
+
+        /* Immediate Execution */
+        /* The query is executed immediately and the result is stored in a list */
+        // Using .ToList() here causes immediate execution, meaning the query is evaluated and results are stored in a list at this point.
+        var evenNumbersImmediate = (from N in numbers where N % 2 == 0 select N).ToList();
+        numbers.Add(14); // Adding a new number to the list
+        foreach (var item in evenNumbersImmediate)
+            Console.WriteLine(item); // The new number will not be included in the result
             
 
         #endregion
