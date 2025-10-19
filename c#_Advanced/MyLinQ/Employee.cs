09cb1503fb5deffa8046abee990f7207f3222afc
Ahmed Fathy <ahmedfathy11876@gmail.com>
2025-10-19 05:38:03 +0300

    Update Interfaces inside the Employee class with its implementation

--- c#_Advanced/MyLinQ/Employee.cs
+++ c#_Advanced/MyLinQ/Employee.cs
@@ -1,28 +1,55 @@
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata;
+
 namespace MyLinQ;
 
-// public class Employee
-// {
-//     public int Id { get; set; }
-//     public string Name { get; set; }
-//     public int Age { get; set; }
-//     public int Salary { get; set; }
-//     
-//     public override bool Equals(object? obj)
-//     {
-//         if (obj == null || GetType() != obj.GetType())
-//             return false;
-//
-//         Employee other = (Employee)obj;
-//         return Id == other.Id;
-//     }
-//
-//     public override int GetHashCode()
-//     {
-//         return Id.GetHashCode();
-//     }
-//
-//     public override string ToString()
-//     {
-//         return $"Employee[Id={Id}, Name={Name}, Age={Age}, Salary={Salary}]";
-//     }
-// }
+public class Employee : IEquatable<Employee> , IEnumerable<Employee> , IEqualityComparer<Employee>
+{
+    public Employee()
+    {
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Salary { get; set; }
+
+    public bool Equals(Employee? other)
+    {
+        if (other is null) return false;
+        return this.Id == other.Id && this.Name == other.Name && this.Age == other.Age && this.Salary == other.Salary;
+    }
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != typeof(Employee)) return false;
+        Employee emp = (Employee)obj;
+        return this.Id == emp.Id && this.Name == emp.Name && this.Age == emp.Age && this.Salary == emp.Salary;
+    }
+
+    public bool Equals(Employee? x, Employee? y)
+    {
+        if (x is null || y is null) return false;
+        return x.Id == y.Id && x.Name == y.Name && x.Age == y.Age && x.Salary == y.Salary;
+    }
+
+    public IEnumerator<Employee> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetHashCode([DisallowNull] Employee obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
--- c#_Advanced/MyLinQ/Program.cs
+++ c#_Advanced/MyLinQ/Program.cs
@@ -4,50 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        // #region C# KeyWords
+        #region C# KeyWords
 
-        // // // The var keyword don`t has something special like int , string 
-        // // // can`t be initialized as a null or not initialized must be initialized 
-        // // // not most properly secured , Implicitly data type
-        // // var name = "Ahmed";
-        //
-        // // // dynamic don`t need have to be defined 
-        // // dynamic name2 = "Ahmed";
-        //
-        //
-        // // // anonymous Type 
-        // // var person = new { name = "Ahmed", age = 20 };
-        // // var person2 = new { Name = "Ahmed", age = 20 };
-        // // Console.WriteLine(person.Equals(person2));
-        // // Console.WriteLine(person.GetType());
-        //
-        // /* person.name = "Fatma";   no option to change the Value of the anonymous data type after th  e creation */
-
-
+        // The var keyword don`t has something special like int , string
+        // can`t be initialized as a null or not initialized must be initialized 
+        // not most properly secured , Implicitly data type
+        var name = "Ahmed";
+        
+        // dynamic don`t need have to be defined 
+        dynamic name2 = "Ahmed";
+        System.Console.WriteLine($"Name : {name2}");
+        
+        // anonymous Type 
+        // var person = new { name = "Ahmed", age = 20 };
+        // var person2 = new { Name = "Ahmed", age = 20 };
+        // Console.WriteLine(person.Equals(person2));
+        // Console.WriteLine(person.GetType());
+        
+        /* person.name = "Fatma";   no option to change the Value of the anonymous data type after th  e creation */
 
 
 
 
-        // #endregion
+
+
+        #endregion
+    
 
         #region LINQ!
+        Employee employee = new Employee();
 
         #region Fluent Syntax
 
         /* Fluent syntax Methods as a class member from Enumerable */
         List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        var result = numbers.Where(number => number % 2 == 0);
-        foreach (var item in result)
-            Console.WriteLine(item);
+        List<int> result = numbers.Where(number => number % 2 == 0).ToList();
+        result.ForEach(item => Console.WriteLine(item));
+        // Call the LINQ method thorugh the Enumerable class
+        var result1 = Enumerable.Where(numbers, numbers => numbers % 2 == 0);
+        // System.Console.WriteLine("Fluent Syntax Result:");
+        // foreach (var item in result)
+        //     Console.WriteLine(item);
 
-
+        // System.Console.WriteLine("Fluent Syntax Result1:");
+        // foreach (var item in result1)
+        //     Console.WriteLine(item);
         #endregion
 
         #region Query Syntax
         /* Easier when using Join Or GroupBy */
         var result01 = from N in numbers where N % 2 != 0 select N;
-        foreach (var item in result01)
-            Console.WriteLine(item);
+        // foreach (var item in result01)
+        //     Console.WriteLine(item);
 
 
         #endregion
@@ -56,16 +64,16 @@ class Program
         /* The query is not executed until you iterate over it */
         var query = from N in numbers where N % 2 == 0 select N;
         numbers.Add(12); // Adding a new number to the list
-        foreach (var item in query)
-            Console.WriteLine(item);
+        // foreach (var item in query)
+        //     Console.WriteLine(item);
 
         /* Immediate Execution */
         /* The query is executed immediately and the result is stored in a list */
         // Using .ToList() here causes immediate execution, meaning the query is evaluated and results are stored in a list at this point.
         var evenNumbersImmediate = (from N in numbers where N % 2 == 0 select N).ToList();
         numbers.Add(14); // Adding a new number to the list
-        foreach (var item in evenNumbersImmediate)
-            Console.WriteLine(item); // The new number will not be included in the result
+        // foreach (var item in evenNumbersImmediate)
+        //     Console.WriteLine(item); // The new number will not be included in the result
             
 
         #endregion
