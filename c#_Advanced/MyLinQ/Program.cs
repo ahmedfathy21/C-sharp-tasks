8638f4d671fa45a4aab631e483cc9ff1a44360c2
Ahmed Fathy <ahmedfathy11876@gmail.com>
2025-10-21 04:22:15 +0300

    Update some toturials based regard to LINQ operations

--- c#_Advanced/MyLinQ/Program.cs
+++ c#_Advanced/MyLinQ/Program.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 namespace MyLinQ;
 class Program
 {
-   
+
     static void Main(string[] args)
     {
-        
+
         #region C# KeyWords
-        
+
         // string [] groups = { "Fruits", "Vegetables", "Meat", "Dairy" };
         // IEnumerable<IGrouping<char , string >> queryFoodGroups = from item in groups group item by item[0];
         //
@@ -24,31 +24,79 @@ class Program
         //         Console.WriteLine($"    {item}");
         //     Console.WriteLine();
         // }
-        
 
-        List<Department> DepartmenList = Data.GetDepartments();
-        List<Employee> EmplyeeList = Data.GetEmployess();
-         // var result = EmplyeeList.Select( e => new { e.FirstName, e.LastName, e.Salary , e.Age , e.Id }).GroupBy(item => item.FirstName[0]).ToList();
-         var result = from e in EmplyeeList
-             select new
-             {
-                 FirstName = e.FirstName = " " ,
-                 LastName = e.LastName + "",
-                 Salary = e.Salary,
-                 Age = e.Age,
-                 Id = e.Id,
-             }; 
-         var resultegroup = from emp in result 
-                         group emp by emp.FirstName[0];
-         foreach (var item in resultegroup)
-         {
-             Console.WriteLine(item.Key);
-             foreach (var item1 in item)
-             {
-                 Console.WriteLine($"    {item1.FirstName} {item1.LastName} {item1.Salary} {item1.Age} {item1.Id}" );
-             }
-             Console.WriteLine();
-         }
+        #region Join_syntax
+
+        Employee employee = new Employee
+            { FirstName = "Ahmed", LastName = "Fatma", Salary = 20000m, Age = 25, Id = 1, DepartmentId = 1 };
+        Employee employee1 = new Employee
+            { FirstName = "Samy", LastName = "Morad", Salary = 10000.15m, Age = 23, Id = 2, DepartmentId = 2 };
+        Employee employee2 = new Employee
+            { FirstName = "Sayed", LastName = "Fayez", Salary = 5436.50m, Age = 20, Id = 3, DepartmentId = 5 };
+        Employee employee3 = new Employee
+            { FirstName = "Ahmed", LastName = "Fatma", Salary = 20000m, Age = 25, Id = 1, DepartmentId = 1 };
+
+        Employee[] employees = new Employee[] { employee, employee1, employee2, employee3 };
+        Department department = new Department { LongName = "Human Resource", ShortName = "HR", DepartmentId = 1 };
+        Department department1 = new Department { LongName = "Front-end", ShortName = "FR", DepartmentId = 2 };
+        Department department2 = new Department { LongName = "Back-end", ShortName = "BK", DepartmentId = 3 };
+        Department[] departments = new Department[] { department, department1, department2 };
+
+        var fiteredresult =
+            (from emp in employees
+                join Dep in departments on emp.DepartmentId equals Dep.DepartmentId
+                select new { emp.FirstName, emp.LastName, emp.Salary, emp.Age, emp.Id, Dep.LongName })
+            .DistinctBy(x => x.FirstName).ToList();
+        foreach (var item in fiteredresult)
+            Console.WriteLine(item);
+
+        var filteredquery = (from emp in employees
+            group emp by emp.FirstName
+            into g
+            select new { FirstName = g.Key, Employees = g }).ToList();
+        foreach (var item in filteredquery)
+        {
+            Console.WriteLine($" The first name is :{item.FirstName} ");
+            foreach (var item1 in item.Employees)
+            {
+                Console.WriteLine(item1);
+            }
+            Console.WriteLine();
+        }
+            
+
+    #endregion
+
+        // List<Department> DepartmenList = Data.GetDepartments();
+        // List<Employee> EmplyeeList = Data.GetEmployess();
+        //  var result = EmplyeeList.Select( e => new { e.FirstName, e.LastName, e.Salary , e.Age , e.Id }).OrderBy(item => item.Salary).ToList().First();
+        //  var FirstElementAfterFilter = EmplyeeList.Select(e => new Employee
+        //      { FirstName = e.FirstName, LastName = e.LastName, Salary = e.Salary, Age = e.Age, Id = e.Id }).ToArray();
+        //  Console.WriteLine(FirstElementAfterFilter[0]);
+        // while (FirstElementAfterFilter.MoveNext())
+        //  Console.WriteLine(FirstElementAfterFilter.Current);
+         // foreach ( var item in result)
+             // Console.WriteLine(var);
+         // var result1 = from e in EmplyeeList
+         //     select new
+         //     {
+         //         FirstName = e.FirstName = " " ,
+         //         LastName = e.LastName + "",
+         //         Salary = e.Salary,
+         //         Age = e.Age,
+         //         Id = e.Id,
+         //     }; 
+         // var resultegroup = from emp in result1 
+         //                 group emp by emp.FirstName[0];
+         // foreach (var item in resultegroup)
+         // {
+         //     Console.WriteLine(item.Key);
+         //     foreach (var item1 in item)
+         //     {
+         //         Console.WriteLine($"    {item1.FirstName} {item1.LastName} {item1.Salary} {item1.Age} {item1.Id}" );
+         //     }
+         //     Console.WriteLine();
+         // }
          Console.ReadKey();
         
         // var FilteredEmployee = EmplyeeList.Where(employee => employee.FirstName == "Ahmed").ToList();
